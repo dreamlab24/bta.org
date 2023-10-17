2953cb53e1773922dceef8c7318aafafdff86666
dreamlabs24 <mccuemedialabs@gmail.com>
2023-10-17 15:57:33 -0400

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `bta.org`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
